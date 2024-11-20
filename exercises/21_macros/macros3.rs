9f592c25abb4c1e928ba0a1d7cf782b3b163f18a
Webrowse <great.adarsh@gmail.com>
2024-11-20 23:55:11 +0530

    \#[macro_use] enable the macro usage int he mod-module

--- exercises/21_macros/macros3.rs
+++ exercises/21_macros/macros3.rs
@@ -1,11 +1,14 @@
 // TODO: Fix the compiler error without taking the macro definition out of this
 // module.
+#[macro_use]
 mod macros {
+    //  #[macro_export]
     macro_rules! my_macro {
         () => {
             println!("Check out my macro!");
         };
     }
+    
 }
 
 fn main() {
