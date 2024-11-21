b44e30e909ec7b9018fa167c5083536b07d484b7
Webrowse <great.adarsh@gmail.com>
2024-11-21 21:58:03 +0530

    Macros exercise finished, 85/94 finished

--- exercises/21_macros/macros4.rs
+++ exercises/21_macros/macros4.rs
@@ -3,13 +3,17 @@
 macro_rules! my_macro {
     () => {
         println!("Check out my macro!");
-    }
+    };
     ($val:expr) => {
         println!("Look at this other macro: {}", $val);
+    };
+    ($a:ty, $b:ty) => {
+        println!("Type is implemented");
     }
 }
 
 fn main() {
     my_macro!();
     my_macro!(7777);
+    my_macro!(i32, i32);
 }
