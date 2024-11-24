659d6c9e68418914f0fc9ef53ba365368ed2216c
Webrowse <great.adarsh@gmail.com>
2024-11-24 06:27:27 +0530

    split, parse::<u8>(), if let Ok(age)

--- exercises/23_conversions/from_into.rs
+++ exercises/23_conversions/from_into.rs
@@ -34,8 +34,19 @@ impl Default for Person {
 // 5. Parse the second element from the split operation into a `u8` as the age.
 // 6. If parsing the age fails, return the default of `Person`.
 impl From<&str> for Person {
-    fn from(s: &str) -> Self {}
-}
+    fn from(s: &str) -> Self {
+        let parts: Vec<&str> = s.split(",").collect();
+        if parts.len() == 2 {
+            let name = parts[0].trim().to_string();
+            if name.is_empty() { return Person::default()}
+            // else{return Person::default()}
+            if let Ok(age) = parts[1].trim().parse::<u8>(){
+                return Person { name, age };
+            }
+        }
+        Person::default()}
+    }
+
 
 fn main() {
     // Use the `from` function.
